be1b8043cfd7694608cd1fb50dec7883c711d3d7
Timothy Sullivan <tsullivan@elastic.co>
2020-03-29 11:51:04 -0700

    Updates

---
+++ precache-manifest.5ad2d72bb525f9165353c9972070975f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b1d308ce9e23241286eef6bfe9ec42",
+    "url": "/vietnamese/index.html"
+  },
+  {
+    "revision": "ef7cd5e516245fcad6c7",
+    "url": "/vietnamese/static/js/2.10a0e1ae.chunk.js"
+  },
+  {
+    "revision": "baf6f38f8697edd0ee08",
+    "url": "/vietnamese/static/js/main.26df190c.chunk.js"
+  },
+  {
+    "revision": "d75ab007593db6b76e29",
+    "url": "/vietnamese/static/js/runtime-main.342195bb.js"
+  }
+]);
